f025fdc3ee2d8b905094612d8a647ac9802b0428
prateekxaxa <prateekxaxa@microsoft.com>
2024-09-12 01:58:35 +0530

    Improved windows service functions

--- service-installer-multios.py
+++ service-installer-multios.py
@@ -1,5 +1,8 @@
 import subprocess
 import platform
+import os
+import shutil
+import time
 
 SERVICE_NAME = "ican-machine-identity"
 LINUX_SERVICE_EXECUTABLE_PATH = "ican-machine-identity"
@@ -47,6 +50,13 @@ def uninstall_systemd_service(service_name):
     subprocess.run(["rm", f"/etc/systemd/system/{service_name}.service"])
     subprocess.run(["systemctl", "daemon-reload"])
 
+# Check if the systemd service files is present or not.
+def check_linux_service_installation_status():
+    """Checks the systemd service files is present or not."""
+
+    service_file_path = f"/etc/systemd/system/{SERVICE_NAME}.service"
+    return os.path.exists(service_file_path)
+
 # Check status of a systemd service.
 def check_systemd_service_status(service_name):
     """Checks the status of a systemd service."""
@@ -89,38 +99,112 @@ def check_linux_service_status():
 def install_windows_service():
     """Installs a windows service."""
 
+    folder_path = f"C:\\Program Files\\{SERVICE_NAME}"
+
+    if os.path.exists(folder_path):
+        shutil.rmtree(folder_path)
+        time.sleep(0.6)
+
     # Create a folder in the program files directory.
-    subprocess.run(["mkdir", f"C:\\Program Files\\{SERVICE_NAME}"])
+    os.makedirs(folder_path)
 
     # Copy the executable to the program files directory using copy.
-    subprocess.run(["copy", WINDOWS_SERVICE_EXECUTABLE_PATH, f"C:\\Program Files\\{SERVICE_NAME}\\"])
+    shutil.copy2(WINDOWS_SERVICE_EXECUTABLE_PATH, f"C:\\Program Files\\{SERVICE_NAME}\\")
+
+    # Get pwd.
+    pwd = os.getcwd()
+
+    # Append with nssm.exe.
+    nssm_path = os.path.join(pwd, "nssm.exe")
 
     # Create a service using nssm.
-    subprocess.run(["nssm", "install", SERVICE_NAME, f"C:\\Program Files\\{SERVICE_NAME}\\ican-machine-identity.exe"])
+    subprocess.run([nssm_path, "install", SERVICE_NAME, f"C:\\Program Files\\{SERVICE_NAME}\\ican-machine-identity.exe"])
 
-    print(f"Service {SERVICE_NAME} installed successfully.")
+    # Start the service.
+    ret = subprocess.run([nssm_path, "start", SERVICE_NAME])
+
+    status = False
+    for _ in range(10):
+        time.sleep(0.5)
+        status = check_windows_service_status()
+        if status:
+            break
+
+    if status:
+        print(f"Service {SERVICE_NAME} started successfully.")
+    else:
+        print(f"Service {SERVICE_NAME} failed to start.")
 
 # Uninstall a windows service.
 def uninstall_windows_service():
     """Uninstalls a windows service."""
 
-    subprocess.run(["nssm", "remove", SERVICE_NAME])
+    # Get pwd.
+    pwd = os.getcwd()
 
-    subprocess.run(["rmdir", f"C:\\Program Files\\{SERVICE_NAME}", "/s", "/q"])
+    # Append with nssm.exe.
+    nssm_path = os.path.join(pwd, "nssm.exe")
+
+    subprocess.run([nssm_path, "stop", SERVICE_NAME])
+
+    for _ in range(10):
+        if not check_windows_service_status():
+            break
+        time.sleep(0.5)
+        subprocess.run([nssm_path, "stop", SERVICE_NAME])
+
+    subprocess.run([nssm_path, "remove", SERVICE_NAME, "confirm"])
+
+    shutil.rmtree(f"C:\\Program Files\\{SERVICE_NAME}")
 
     print(f"Service {SERVICE_NAME} uninstalled successfully.")
 
-# Check status of a windows service.
-def check_windows_service_status():
+# Check installation status of a windows service.
+def check_windows_service_installation_status():
     """Checks the status of a windows service."""
 
-    return subprocess.run(["nssm", "status", SERVICE_NAME], capture_output=True, text=True).returncode
+    # Get pwd.
+    pwd = os.getcwd()
+
+    # Append with nssm.exe.
+    nssm_path = os.path.join(pwd, "nssm.exe")
+
+    return subprocess.run([nssm_path, "status", SERVICE_NAME], capture_output=True, text=True).returncode
+
+# Check if string matches with nssm output.
+def nssm_contains(nssm_output, string_value):
+    if string_value in nssm_output:
+        return True
+    
+    nssm_format = ''.join([el for (idx, el) in enumerate(nssm_output) if idx % 2 == 1])
+
+    if string_value in nssm_format:
+        return True
+    
+    nssm_format = ''.join([el for (idx, el) in enumerate(nssm_output) if idx % 2 == 0])
+
+    return string_value in nssm_format
+
+# Check installation status of a windows service.
+def check_windows_service_status():
+    """Check installation status of a windows service"""
+    
+    # Get pwd.
+    pwd = os.getcwd()
+
+    # Append with nssm.exe.
+    nssm_path = os.path.join(pwd, "nssm.exe")
+
+    ret = subprocess.run([nssm_path, "status", SERVICE_NAME], capture_output=True, text=True)
+    return nssm_contains(ret.stdout, "SERVICE_RUNNING")
+
 
 # Main.
 print("Welcome to the Service Installer Multi OS")
 print("1. Install service")
 print("2. Uninstall service")
-print("3. Check service status")
+print("3. Check service installation status")
+print("4. Check service running status")
 ch = input("Enter your choice: ")
 
 if ch == "1":
@@ -150,6 +234,25 @@ elif ch == "3":
     print("Checking service status...")
     if platform.system() == "Linux":
         if is_systemd_available():
+            status = check_linux_service_installation_status()
+            if status:
+                print(f"Service {SERVICE_NAME} is running.")
+            else:
+                print(f"Service {SERVICE_NAME} is not running.")
+        else:
+            print("Systemd is not available. Aborting status check.")
+    elif platform.system() == "Windows":
+        status = check_windows_service_installation_status()
+        if status:
+            print(f"Service {SERVICE_NAME} is running.")
+        else:
+            print(f"Service {SERVICE_NAME} is not running.")
+    else:
+        print("Unsupported OS.")
+elif ch == "4":
+    print("Checking service running status...")
+    if platform.system() == "Linux":
+        if is_systemd_available():
             status = check_linux_service_status()
             if status == 0:
                 print(f"Service {SERVICE_NAME} is running.")
@@ -159,7 +262,7 @@ elif ch == "3":
             print("Systemd is not available. Aborting status check.")
     elif platform.system() == "Windows":
         status = check_windows_service_status()
-        if status == 0:
+        if status:
             print(f"Service {SERVICE_NAME} is running.")
         else:
             print(f"Service {SERVICE_NAME} is not running.")
